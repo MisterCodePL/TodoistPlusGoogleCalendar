dee3052aeac2974596051831f9747f08f0d8fdf4
MisterCodePL <mistercode11@gmail.com>
2017-11-06 00:32:37 +0100

    Created project.

---
+++ TodoistPlusGoogleCalendar/ImportFromGoogleCalendar.cs
@@ -0,0 +1,22 @@
+using System.Linq;
+using System.Net;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace TodoistPlusGoogleCalendar
+{
+    public static class ImportFromGoogleCalendar
+    {
+        [FunctionName("ImportFromGoogleCalendar")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            throw new NotImplementedException();
+        }
+    }
+}
